a7d962146bf21c8408e4b358d234a3e8c2e46e69
berndbousard <bernd.bousard@student.howest.be>
2017-01-21 15:17:32 +0100

    initial commit

---
+++ app/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <Text>Schrijf hier je code</Text>
+    );
+  }
+}
